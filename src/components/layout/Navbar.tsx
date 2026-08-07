f240f74c79f3eccae16c91b58d5f5efd5c2d2579
Felix-au <felixaugum@gmail.com>
2026-08-08 02:31:37 +0530

    feat(layout): move email link icon above resume icon in sidebar and mobile nav

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -113,6 +113,17 @@ export const Navbar: React.FC = () => {
 
         {/* Bottom Social Icon Links */}
         <div className={styles.navIcons}>
+          {/* Email */}
+          <a
+            className={styles.navIconLink}
+            href="mailto:felixaugum@gmail.com"
+            aria-label="Email"
+          >
+            <svg className={styles.navIcon} viewBox="0 0 24 24">
+              <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+            </svg>
+          </a>
+
           {/* Resume */}
           <a
             className={styles.navIconLink}
@@ -154,17 +165,6 @@ export const Navbar: React.FC = () => {
               <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77Z" />
             </svg>
           </a>
-
-          {/* Email */}
-          <a
-            className={styles.navIconLink}
-            href="mailto:felixaugum@gmail.com"
-            aria-label="Email"
-          >
-            <svg className={styles.navIcon} viewBox="0 0 24 24">
-              <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
-            </svg>
-          </a>
         </div>
       </nav>
 
@@ -187,6 +187,17 @@ export const Navbar: React.FC = () => {
 
         {/* Mobile Overlay Bottom Socials */}
         <div className={styles.mobileSocials}>
+          {/* Email */}
+          <a
+            className={styles.navIconLink}
+            href="mailto:felixaugum@gmail.com"
+            aria-label="Email"
+          >
+            <svg className={styles.navIcon} viewBox="0 0 24 24">
+              <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+            </svg>
+          </a>
+
           {/* Resume */}
           <a
             className={styles.navIconLink}
@@ -223,15 +234,6 @@ export const Navbar: React.FC = () => {
           >
             <svg className={styles.navIcon} viewBox="0 0 24 24">
               <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77Z" />
-            </svg>
-          </a>
-          <a
-            className={styles.navIconLink}
-            href="mailto:felixaugum@gmail.com"
-            aria-label="Email"
-          >
-            <svg className={styles.navIcon} viewBox="0 0 24 24">
-              <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
             </svg>
           </a>
         </div>
